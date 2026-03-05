a392b22301efded57db5dd2e81b41b0eacfe71cf
PC <PC@DESKTOP-2MKAN0E>
2026-03-06 00:18:31 +0900

    category-scroll.js 파일 수정

--- src/main/webapp/js/category-scroll.js
+++ src/main/webapp/js/category-scroll.js
@@ -2,12 +2,16 @@
  *  메인(index.jsp) 상단부 카테고리 영역 스크롤 기능 수정
  */
 
-const scrollBox = document.querySelector(".category-scroll");
+document.addEventListener("DOMContentLoaded", function(){
 
-document.querySelector(".scroll-btn.left").addEventListener("mouseenter", ()=>{
-    scrollBox.scrollBy({ left:-300, behavior:"smooth" });
-});
+	const scrollBox = document.querySelector(".category-scroll");
+	
+	document.querySelector(".scroll-btn.left").addEventListener("mouseenter", ()=>{
+	    scrollBox.scrollBy({ left:-300, behavior:"smooth" });
+	});
+	
+	document.querySelector(".scroll-btn.right").addEventListener("mouseenter", ()=>{
+	    scrollBox.scrollBy({ left:300, behavior:"smooth" });
+	});
 
-document.querySelector(".scroll-btn.right").addEventListener("mouseenter", ()=>{
-    scrollBox.scrollBy({ left:300, behavior:"smooth" });
 });
