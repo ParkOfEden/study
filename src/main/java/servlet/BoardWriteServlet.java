68ad2cf7b27d4b51cdc47af838130ca417ddc940
PC <PC@DESKTOP-2MKAN0E>
2026-03-06 01:17:34 +0900

    파일 업로드 관련 servlet 추가

--- src/main/java/servlet/BoardWriteServlet.java
+++ src/main/java/servlet/BoardWriteServlet.java
@@ -59,6 +59,8 @@ public class BoardWriteServlet extends HttpServlet {
         vo.setContent(p_desc);  // VO 내부 변수명이 content라도 값은 p_desc를 넣음
         vo.setSystem_filename(system_filename); 
         vo.setPrice(price);     // 반드시 BoardVO에 setPrice 메서드가 있어야 함
+        
+        vo.setSystem_filename(system_filename); // 파일 업로드 관련
 
         // 4. DAO 호출
         BoardDAO dao = new BoardDAO();
