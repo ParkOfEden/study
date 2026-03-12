2fa123219ec8ed0886bbb904a70ab6d1d19c7588
KTE <KTE@DESKTOP-KTE50x>
2026-03-12 17:00:49 +0900

    사용되지 않는 import 주석 처리

--- src/main/java/utils/GmailAuthenticator.java
+++ src/main/java/utils/GmailAuthenticator.java
@@ -1,8 +1,8 @@
 package utils;
 
-import java.io.FileNotFoundException;
+/*import java.io.FileNotFoundException;*/
 import java.io.FileReader;
-import java.io.IOException;
+/*import java.io.IOException;*/
 import java.util.Properties;
 
 import jakarta.mail.Authenticator;
